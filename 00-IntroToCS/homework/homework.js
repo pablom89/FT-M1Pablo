4be05ca76cec47d4dd58d91821de2c9268b691fb
pablom <pablo_89_cba@hotmail.com>
2021-12-06 18:21:19 -0300

    First commit

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -2,11 +2,23 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  var res = 0;
+  var arreglo = num.split("").reverse();
+  for (var i = 0; i < arreglo.length; i++) {
+    res = res + Number(arreglo[i]) * Math.pow(2, i);
+  }
+  return res;
 
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  var binario = (num % 2).toString();
+  while (num > 1) {
+    num = parseInt(num / 2);
+    binario = (num % 2) + binario;
+  }
+  return binario;
 
 }
 
